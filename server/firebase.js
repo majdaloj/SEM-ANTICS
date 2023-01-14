ad14aee8decf7443c2444a16eaf358a4adc4bd7a
Majda Lojpur <majda.lojpur@mail.utoronto.ca>
2023-01-14 16:27:10 -0500

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY;,
+  apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
