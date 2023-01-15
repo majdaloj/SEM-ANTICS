7307d8bc59977255859ecf83dee192bfb7e16a00
faizahsayyid <faizah.sayyid@gmail.com>
2023-01-14 22:40:25 -0500

    feat: working on drafts ui

--- client/src/pages/AllDrafts.jsx
+++ client/src/pages/AllDrafts.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useEffect } from 'react'
+import Draft from '../components/Draft'
 
 function AllDrafts() {
+
+  const drafts = [1, 2, 3, 4, 5, 6]
+
   return (
-    <div>AllDrafts</div>
+    <>
+        <h1 className='text-center mb-4'>Past Drafts</h1>
+        {
+          drafts.map((classifications) => {
+            return <div className='mb-3 w-75 mx-auto form-control'>Hello</div>
+          })
+        }
+    </>
   )
 }
 
